b0597a9280e273437ded11d52c18056b4c7b3a0d
Lukas_Schaub <lukas_schaub@gmx.ch>
2021-07-27 08:51:33 +0200

    updated 00_simple_monod

--- 00_monod_simple/dynamicCellGrowth.C
+++ 00_monod_simple/dynamicCellGrowth.C
@@ -3,14 +3,16 @@
 \*--------------------------------*/
 
 #include "fvCFD.H"
+#include <math.h>
 // here more needs to be added most likely
 
 int main(int argc, char *argv[])
 {
 	// used variables in code below
-	int i; 		// for for loop
-	int x0;		// initial cell concentration
-	float muemax; 	// muemax
+	int i; 			// for for loop
+	int x0;			// initial cell concentration
+	float muemax; 		// muemax
+	double exp(double x);	// initializing the exponential function
 	Info << "Testing something with cells. " << endl;
 	// here some caluclaiton will now happen
 	// useing just some testing numbers
@@ -21,7 +23,7 @@ int main(int argc, char *argv[])
 
 	for (i = 0; i < 100; i++)
 	{
-		Info << "Time = " << i << nl << endl;
-		Info << "Cells = " << x0*muemax*i << nl << endl; // wrong procedure to calculate values check more examples
+		Info << "Time  = " << i << endl;
+		Info << "Cells = " << x0*exp(muemax*i) << nl << endl; // wrong procedure to calculate values check more examples
 	}
 }
