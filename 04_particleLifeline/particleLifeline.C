25aa6c5d91245cfeccf8ada363f0e22a432d228d
LS-2ednar <lukas_schaub@gmx.ch>
2021-08-25 15:22:26 +0200

    case 04 currently nneds multiple functions to be executed -> improve it please

---
+++ 04_particleLifeline/particleLifeline.C
@@ -0,0 +1,100 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) 2011-2020 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    rhoParticleFoam
+
+Description
+    Transient solver for the passive transport of a single kinematic
+    particle cloud, with optional mesh motion and mesh topology changes.
+
+    Uses a pre-calculated velocity field to evolve the cloud.
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include "dynamicFvMesh.H"
+#include "singlePhaseTransportModel.H"
+#include "kinematicMomentumTransportModel.H"
+#include "basicKinematicCollidingCloud.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+    argList::addOption
+    (
+        "cloudName",
+        "name",
+        "specify alternative cloud name. default is 'kinematicCloud'"
+    );
+
+    #define NO_CONTROL
+    #include "postProcess.H"
+
+    #include "setRootCaseLists.H"
+    #include "createTime.H"
+    #include "createDynamicFvMesh.H"
+    #include "createFields.H"
+    #include "CourantNo.H"
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    Info<< "\nStarting time loop\n" << endl;
+
+    while (runTime.loop())
+    {
+        Info<< "Time = " << runTime.timeName() << nl << endl;
+
+        Info<< "\nStoring Cloudpositions:\n " <<endl;
+	Info << kinematicCloud.storeGlobalPositions(); << endl; //not sure if this works but we shell see
+
+        mesh.update();
+
+        if (mesh.changing())
+        {
+            U.correctBoundaryConditions();
+        }
+
+        laminarTransport.correct();
+
+        Info<< "Evolving " << kinematicCloud.name() << endl;
+        mu = laminarTransport.nu()*rhoInfValue;
+
+        kinematicCloud.evolve();
+
+        runTime.write();
+
+        Info<< "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
+            << "  ClockTime = " << runTime.elapsedClockTime() << " s"
+            << nl << endl;
+    }
+
+    Info<< "End\n" << endl;
+    
+    Info<< "For individual Particle Tracking run particleTracks floowed by VTK_to_IndividualParticles.py"<<endl;
+    return 0;
+}
+
+
+// ************************************************************************* //
--- README.md
+++ README.md
@@ -35,13 +35,7 @@ The Cell Growth is determined prior to an the calculation of of the flowfield (u
 Here the pimple algorithm is modified to work with specific values which are added to an "cellProperties" dictionary and calculate the value of cells and substrate after every iteration. To archive this the pimpleFoam solver application is copyied then the .C file is renamed and adjusted in the Make/files file. Then an additional TestEqn.H is introduced where the calculation of cells and substrate is done. Note that at this stage the calculated values are not reflecting the acctual values. This is due to the way the function is updated every timestep. Further, the outputfile no just shows the last calculated value which needs some more modifications.
 
 ### Fifth example
-Introducing a new class called "biocell" which is based on the particle class and which should work together with the new application which is called "biocellFoam". To simulate the location of particles after every timestep.
-
-
-### Fith example
-This example is based on this [PDF from the "OpenFOAM 3 Weeks" series](http://www.tfd.chalmers.se/~hani/kurser/OS_CFD_2020/lectureNotes/14_implementSolidParticlesInVOF.pdf). It is a great example on how to deal with the modification of existing code and points at the essential steps that are needed to modifiy the final results. Further, this example introduces particles which are a step closer to the final goal.
-
-
+Here the "particleFoam" solver is used to first simulate the different particles in a given test case. Then an additional step is taken and the postprocessing technique "particleTracks" which needs an additional constant/particleTrackProperties file are used to generate a VTK file. Then a python script was introduced which allows the reorganization of the vtk file in a way, that the individual particle tracks are saved at the same spot. The file is not jet optimized and further testing needs to be done with it.
 
 
 ***
