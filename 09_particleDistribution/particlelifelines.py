0faa31e88b3565040c4200d040bcc1d17c1ef4aa
LS-2ednar <lukas_schaub@gmx.ch>
2021-09-14 10:43:27 +0200

    removed unnecessary code, commentent code, and implemented progressbar

--- 09_particleDistribution/particlelifelines.py
+++ 09_particleDistribution/particlelifelines.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Sep  8 12:58:46 2021
-
-@author: Lukas Schaub
+Generate the Particlelifelines and the state transion 
+probability matricies for each particle as well as a 
+long term steady state of the system.
 """
 import os
+import sys
 import pickle
 from get_Particles import Particle
 import numpy as np
@@ -48,6 +49,7 @@ def read_zones(path):
             values.append(int(b[i]))
                  
     return values
+
 def read_locations(path):
     """
     Read coordinates of cell centres form a file
@@ -76,59 +78,99 @@ def read_particledata(particledata):
             filehandle.close()
             return particles, particle_space
 
-def particle_lifelines(particle_list,cell_list,zones,num_zones):
+def particle_lifelines(particle_list,cell_list,zone_list,num_zones):
+    """
+    Generates the particles history in reagards to the values it had to endure.
     
+    Parameters:
+    particle_list --> list of Particle
+    cell_list     --> list of lists
+    zone_list     --> list of integers
+    num_zones     --> integer
+    
+    Output:
+    end_particles --> list of Particle
+                      These Particles have now stroed information about there
+                      value_history which shows where they where exposed to
+                      the different zones.
+    """
+    #for each particle get the closest point from the list of cell centeres.
     end_particles = []
+    
+    #for progressbar
+    c, pb = 0,0
+    print('Mapping particles to cell-centers for zone determination')
     for particle in particle_list:
+        c+= 1
         for i in range(len(particle.x)):
             points_ = []
             for point in cell_list:
                 points_.append(((point[0]-particle.x[i])**2+(point[1]-particle.y[i])**2+(point[2]-particle.z[i])**2)**(1/2))
             particle.value_index.append(points_.index(min(points_)))
         end_particles.append(particle)
-    
+        
+        if c == len(particle_list)//50:
+                pb += 1
+                sys.stdout.write('\r')
+                sys.stdout.write("[%-50s] %d%%" % ('='*pb, 2*pb))
+                sys.stdout.flush()
+                c = 0
+            
+            
+    #match indecies to zone values
     for particle in particle_list:
         for i in particle.value_index:
-            particle.value_history.append(zones[i])
-        
-        zone_dist = {}
-        for key in range(1,num_zones+1):
-            zone_dist[key] = 0
-            for element in particle.value_history:
-                if element == key:
-                    zone_dist[key] += 1
-            zone_dist[key] = zone_dist[key]/len(particle.x)
+            particle.value_history.append(zone_list[i])
+            
+        # #might be used later
+        # zone_dist = {}
+        # for key in range(1,num_zones+1):
+        #     zone_dist[key] = 0
+        #     for element in particle.value_history:
+        #         if element == key:
+        #             zone_dist[key] += 1
+        #     zone_dist[key] = zone_dist[key]/len(particle.x)
                     
-        particle.zone_dist = zone_dist
+        # particle.zone_dist = zone_dist
         
     return end_particles
 
 def particle_state_transions(list_of_particles,num_states):
+    """
+    Given a list_of_particles and a number of available states (num_states) a state
+    transion matrix can be caluclated and is added to each particle.
+    """
     
     for particle in list_of_particles:
-        # print(particle)
         #get the list of states for a particle
         list_of_states = particle.value_history
-        out = np.zeros((num_states,num_states)) #numpy
         
+        #iniate a nxn zero matrix where n is equal to num_states
+        out = np.zeros((num_states,num_states)) 
+        
+        #increment values in the nxn matrix 
         for i in range(len(list_of_states)-1):
             curr_ = list_of_states[i]
             next_ = list_of_states[i+1]
             out[curr_-1][next_-1] += 1
         
+        # get percentages for each value in the system
         for n in range(len(out)):
             line_sum = sum(out[n])
             for m in range(len(out)):
                 
                 if out[n][m] != 0:
                     out[n][m] = out[n][m]/line_sum
-                
+        
+        #add the state_transition_probability matrix to the particle
         particle.state_transions = out
     return list_of_particles
 
 def system_state_transion(list_of_particles, Number_of_states):
-    
-    P = np.zeros((Number_of_states,Number_of_states)) 
+    """
+    Given a list_of_particles the systems average state-transion-probability-matrix (P) is calculated
+    """
+    P = np.zeros((len(list_of_particles[0].state_transions),len(list_of_particles[0].state_transions))) 
     for particle in list_of_particles:
         P+=particle.state_transions
         
@@ -137,7 +179,7 @@ def system_state_transion(list_of_particles, Number_of_states):
             P[i] = P[i]/sum(P[i])
     
     return P
-
+ 
 def long_term_steady_state(P):
     """
     Assuming Av=b where v is the steady-state-transion probability
@@ -146,19 +188,18 @@ def long_term_steady_state(P):
     b is equal to a column vector of shape n+1 where all values are 0 but the last one which is equal to 1
     """
     
-    A = np.append(np.transpose(P)-np.identity(len(P)),[[1,1,1]],axis=0)
-    b = np.transpose(np.array([[0,0,0,1]]))
+    A = np.append(np.transpose(P)-np.identity(len(P)),[np.ones(len(P))],axis=0)
+    b = np.transpose(np.append(np.zeros(len(P)),1))
     
     v = np.linalg.solve(np.transpose(A).dot(A),np.transpose(A).dot(b))
     
     for state in range(len(P)):
         print(f'State {state}: {v[state]}')
     
-    return v
-                  
+    return v    
+             
 if __name__ == '__main__':
     
-
     print('Running Script')
     abspath = os.path.abspath(__file__)
     dname = os.path.dirname(abspath)
@@ -170,21 +211,19 @@ if __name__ == '__main__':
     coordinates = read_locations('0/C')
     particles, particle_space = read_particledata('particle.data')
     
-    print('\nMapping particles to nearest cell center location')
-    #new_particles = map_particles_to_cells(particles,coordinates)
-    new_particles = particles
-    print('\nDetermine the lifelines of the particles')
-    end_particles = particle_lifelines(new_particles,coordinates,zones,number_of_zones)
+    print('\nDetermine the lifelines of the particles\n')
+    end_particles = particle_lifelines(particles,coordinates,zones,number_of_zones)
     end_particles = particle_state_transions(end_particles,number_of_zones)
     
     
     P = system_state_transion(end_particles,3)
-    print(f'\nThe State transion matrix for this problem looks as follows:\n{P}')
+    print(f'\n\nThe State transion matrix for this problem looks as follows:\n\n{P}')
     
     
     print('\n\nWhere the long-term steady-state-solution vector is:\n')
     steady_state = long_term_steady_state(P)
     
+
 ### To Do
 ### Modify the output in lines 79 to 102 --> progressbar or something
 ###
